ea23452ed01f5ba55a6276b22cf8c5b38edfafa8
Allie Lapinski <alex.lapinski@gmail.com>
2025-04-01 13:01:20 -0400

    Added Route Placeholders

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './routes/App.tsx';
+import Contact from './routes/Contact.tsx';
+import Education from './routes/Education.tsx';
+import Experience from './routes/Experience.tsx';
+import Projects from './routes/Projects.tsx';
+import Skills from './routes/Skills.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ErrorPage from './routes/error-page.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  },
+  {
+    path: '/education',
+    element: <Education />
+  },
+  {
+    path: '/experience',
+    element: <Experience />
+  },
+  {
+    path: '/projects',
+    element: <Projects />
+  },
+  {
+    path: '/skills',
+    element: <Skills />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
