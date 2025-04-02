e550c34aa5ba9c29dc1dea807d1cce3fe5d90f68
Allie Lapinski <alex.lapinski@gmail.com>
2025-04-02 10:54:06 -0400

    Added rainbowfern logo to main page

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import rainbowFernLogo from '../assets/fern-colors-web.svg'
 import './App.css'
 
 function App() {
@@ -9,11 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" target="_blank">
+          <img src={rainbowFernLogo} className="logo rainbowfern" alt="Rainbowfern logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
