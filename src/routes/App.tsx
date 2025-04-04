292305df3d1acb749bb84fdd00e0824b88c52487
Allie Lapinski <alex.lapinski@gmail.com>
2025-04-04 18:41:32 -0400

    Added tailwindcss & starting usage of it

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,30 +1,31 @@
 import { useState } from 'react'
 import rainbowFernLogo from '../assets/fern-colors-web.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="#" target="_blank">
-          <img src={rainbowFernLogo} className="logo rainbowfern" alt="Rainbowfern logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="mt-0 mr-auto ml-auto mb-0 relative">
+      <div className="flex-auto rounded-xl bg-white">
+        <div>
+          <a href="#" target="_blank">
+            <img src={rainbowFernLogo} className="logo rainbowfern" alt="Rainbowfern logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
