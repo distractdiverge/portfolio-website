76451fc0c170d5d8f28c6a3cf0e2fdfe25e1a305
Allie Lapinski <alex.lapinski@gmail.com>
2025-04-07 17:56:58 -0400

    Finally extracted out common css between links in nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,25 +3,19 @@ import { NavLink } from "react-router";
 
 const HeaderComponent : React.FC = () => {
 
+    const common = "text-xs md:text-base block p-2";
+    const linkClassname = `${common} hover:text-gray-400 hover:underline`;
+    const buttonClassname = `${common} rounded-md leading-[22px] bg-gray-800 text-white mx-auto hover:text-black hover:bg-gray-400`;
+
     return (
         <header className="bg-white flex justify-between p-1 md:p-4">
             <h1 className="block p-2 text-s md:text-base">Rainbowfern.com</h1>
             <div className="flex">
-                <NavLink 
-                    className="text-xs md:text-base block p-2 hover:text-gray-400 hover:underline"
-                    to="/skills">Skills</NavLink>
-                <NavLink 
-                    className="text-xs md:text-base block p-2 hover:text-gray-400 hover:underline"
-                    to="/projects">Projects</NavLink>
-                <NavLink 
-                    className="text-xs md:text-base block p-2 hover:text-gray-400 hover:underline"
-                    to="/experience">Experience</NavLink>
-                <NavLink 
-                    className="text-xs md:text-base block p-2 hover:text-gray-400 hover:underline"
-                    to="/education">Education</NavLink>
-                <NavLink 
-                    className=" block rounded-md text-xs md:text-base leading-[22px] bg-gray-800 text-white mx-auto p-2 hover:text-black hover:bg-gray-400"
-                    to="/contact">Contact</NavLink>
+                <NavLink className={linkClassname}   to="/skills">Skills</NavLink>
+                <NavLink className={linkClassname}   to="/projects">Projects</NavLink>
+                <NavLink className={linkClassname}   to="/experience">Experience</NavLink>
+                <NavLink className={linkClassname}   to="/education">Education</NavLink>
+                <NavLink className={buttonClassname} to="/contact">Contact</NavLink>
             </div>
         </header>
     );
