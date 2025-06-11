9cf5fa7d7ae83f95807e598c37040ea751414f67
Allie Lapinski <alex.lapinski@gmail.com>
2025-06-11 11:53:17 -0400

    Added google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react'; // Added for PageViewTracker
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import About from './routes/About.tsx';
@@ -8,13 +9,39 @@ import Projects from './routes/Projects.tsx';
 import EducationAndSkills from './routes/EducationAndSkills.tsx';
 import HeaderComponent from './components/header.tsx';
 import FooterComponent from './components/footer.tsx';
+import ReactGA from 'react-ga4'; // Added for Google Analytics
 
 import ErrorPage from './routes/error-page.tsx';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom'; // Added useLocation
+
+// Initialize Google Analytics
+const GA_MEASUREMENT_ID = 'G-5H38H1LX1F'; // Your Measurement ID
+if (GA_MEASUREMENT_ID) {
+  ReactGA.initialize(GA_MEASUREMENT_ID);
+  console.log('Google Analytics Initialized with ID:', GA_MEASUREMENT_ID);
+} else {
+  console.warn('Google Analytics Measurement ID is not set. Tracking will be disabled.');
+}
+
+// Component to track page views
+const PageViewTracker: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (GA_MEASUREMENT_ID) {
+      ReactGA.send({ hitType: "pageview", page: location.pathname + location.search, title: document.title });
+      // Optional: Log to console for debugging during development
+      // console.log(`GA Pageview Sent: ${location.pathname + location.search}, Title: ${document.title}`);
+    }
+  }, [location]); // Re-run when location changes
+
+  return null; // This component does not render anything
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <PageViewTracker /> {/* Added PageViewTracker here */}
       <div className="grid grid-cols-12 gap-0 justify-stretch align-stretch min-h-screen grid-rows-[48px_auto_73px] md:grid-rows-[72px_auto_73px]">
           <div className="col-span-12">
               <HeaderComponent />
